98abcf07fb6a05d25be90369620205d47a503f12
malsherer <mgracesherer@gmail.com>
2023-10-20 16:44:44 -0700

    worked on update functionality

---
+++ src/_tests_/CatEdit.test.js
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom'
+import CatEdit from '../pages/CatEdit'
+
+describe('<CatEdit />', () => {
+    it('renders a heading on cat edit page', () => {
+        render(
+            <BrowserRouter>
+                <CatEdit />
+            </BrowserRouter>
+        )
+
+            const newHeading = screen.getByRole('heading', {
+                name: /update/i
+            }) 
+
+            screen.debug(newHeading)
+            expect(newHeading).toHaveTextContent("Update Your Cat")
+    })
+})
